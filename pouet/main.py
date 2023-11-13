9775d2f2084efb3c2aae6bbbe1fd9697fb8f6275
jfeuillatre <jfeuillatre@jfeuillatre.home>
2023-11-13 12:40:26 +0100

    [PARSING] mid files

--- pouet/main.py
+++ pouet/main.py
@@ -13,16 +13,15 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
+
 def main():
     argparser = argparse.ArgumentParser(description="anotate music sheet")
-    argparser.add_argument("-i", "--infile", default=None, type=str,
-                         help="input filename")
-    argparser.add_argument("-d", "--indir", default=None, type=str,
-                         help="input filename")
-    argparser.add_argument("-o", "--outdir", default=None, type=str,
-                         help="output filename")
-    argparser.add_argument("-ms", "--ms_img_path", default=None, type=str,
-                         help="output filename")
+    argparser.add_argument("-i", "--infile", default=None, type=str, help="file")
+    argparser.add_argument("-d", "--indir", default=None, type=str, help="directory's files")
+    argparser.add_argument("-o", "--outdir", default=None, type=str, help="output directory")
+    argparser.add_argument(
+        "-ms", "--ms_img_path", default=None, type=str, help="path to musescore image"
+    )
     args = argparser.parse_args()
 
     if args.indir:
@@ -32,20 +31,34 @@ def main():
     else:
         logging.error("no input file")
         return False
-    cache_dir = "cache"
+    _location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    cache_dir = _location + "/cache"
     out_dir = args.outdir or "pdf"
 
     for file in files:
         file_name = file.split("/")[-1].split(".")[0]
         ext = file.split(".")[-1]
-        if ext not in ["mid", "mscx", "mscz", "musicxml", "mxl"]:
+        if ext not in ["mid", "mscx", "mscz", "musicxml", "mxl", "MID"]:
             logging.error(f"{file_name} : extension .{ext} not supprted")
             continue
-        if convert(old=file, new=f"{cache_dir}/{file_name}.mscx", ms_img=args.ms_img_path, logger=logging):
-            parsing(f"{cache_dir}/{file_name}.mscx", file_name)
-            convert(old=f"{cache_dir}/{file_name}_note.mscx", new=f"{out_dir}/{file_name}.pdf", ms_img=args.ms_img_path, logger=logging)
-            os.remove(f"{cache_dir}/{file_name}_note.mscx")
-            os.remove(f"{cache_dir}/{file_name}.mscx")
+        if convert(
+            old=args.indir + "/" + file,
+            new=f"{cache_dir}/{file_name}.mscx",
+            ms_img=args.ms_img_path,
+            logger=logging,
+        ):
+            try:
+                parsing(f"{cache_dir}/{file_name}.mscx", file_name)
+                convert(
+                    old=f"{cache_dir}/{file_name}_note.mscx",
+                    new=f"{out_dir}/{file_name}.pdf",
+                    ms_img=args.ms_img_path,
+                    logger=logging,
+                )
+                os.remove(f"{cache_dir}/{file_name}_note.mscx")
+                os.remove(f"{cache_dir}/{file_name}.mscx")
+            except Exception as e:
+                logging.error(f"error on {file_name}: ", str(e))
 
 
 if __name__ == "__main__":
--- pouet/parsing.py
+++ pouet/parsing.py
@@ -2,9 +2,21 @@ import xml.etree.ElementTree as ET
 
 
 def pitch_to_note(pitch):
-    pitch_note_dict = {0: "C", 1: "C#", 2: "D", 3: "D#", 4: "E", 5: "F", 
-        6: "F#",7: "G", 8: "G#", 9: "A", 10: "A#", 11: "B"}
-    if (pitch == -1):
+    pitch_note_dict = {
+        0: "C",
+        1: "C#",
+        2: "D",
+        3: "D#",
+        4: "E",
+        5: "F",
+        6: "F#",
+        7: "G",
+        8: "G#",
+        9: "A",
+        10: "A#",
+        11: "B",
+    }
+    if pitch == -1:
         return "R"
     return pitch_note_dict[int(pitch) % 12]
 
@@ -17,7 +29,8 @@ def trombone_position(pitch):
         4: [43, 50, 55, 59],
         3: [44, 51, 56, 60, 63, 66, 69],
         2: [45, 52, 57, 61, 64, 67, 70, 72, 74, 76, 78],
-        1: [46, 53, 58, 62, 65, 68, 71, 73, 75, 77, 79]}
+        1: [46, 53, 58, 62, 65, 68, 71, 73, 75, 77, 79],
+    }
     for pos in positions:
         if int(pitch) in positions[pos]:
             return pos
@@ -35,15 +48,15 @@ def parsing(path, filename):
             for chord in voice.findall(".//Chord"):
                 position = ""
                 for note in chord.findall(".//Note"):
-                    aspn = pitch_to_note(note.find('.//pitch').text)
-                    position += str(trombone_position(note.find('.//pitch').text))
+                    aspn = pitch_to_note(note.find(".//pitch").text)
+                    position += str(trombone_position(note.find(".//pitch").text))
                 staff_text = ET.Element("StaffText")
                 ET.SubElement(staff_text, "style").text = "Measure Number"
                 text = ET.SubElement(staff_text, "text")
                 font = ET.SubElement(text, "font")
                 font2 = ET.SubElement(text, "font")
-                font.set("size","7")
-                font2.set("face","FreeSans")
+                font.set("size", "7")
+                font2.set("face", "FreeSans")
                 font2.text = f"{position}\n{aspn}"
                 if chord in list(voice):
                     index = list(voice).index(chord)
